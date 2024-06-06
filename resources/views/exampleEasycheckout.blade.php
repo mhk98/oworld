31020c12dfeda81d0fb8f22c29c49b1d56bc0dff
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2024-06-06 14:48:26 +0600

    latest

--- resources/views/exampleEasycheckout.blade.php
+++ resources/views/exampleEasycheckout.blade.php
@@ -38,12 +38,12 @@
     </div> -->
 
     <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
-            <!-- <h4 class="d-flex justify-content-between align-items-center mb-3">
+        <!-- <div class="col-md-4 order-md-2 mb-4">
+            <h4 class="d-flex justify-content-between align-items-center mb-3">
                 <span class="text-muted">Your cart</span>
                 <span class="badge badge-secondary badge-pill">3</span>
-            </h4> -->
-            <!-- <ul class="list-group mb-3">
+            </h4>
+            <ul class="list-group mb-3">
                 <li class="list-group-item d-flex justify-content-between lh-condensed">
                     <div>
                         <h6 class="my-0">Product name</h6>
@@ -69,8 +69,8 @@
                     <span>Total (BDT)</span>
                     <strong>1200 TK</strong>
                 </li>
-            </ul> -->
-        </div>
+            </ul>
+        </div> -->
         <div class="col-md-8 order-md-1">
             <!-- <h4 class="mb-3">Billing address</h4> -->
             <form method="POST" class="needs-validation" novalidate>
